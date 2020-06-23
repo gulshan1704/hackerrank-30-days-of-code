8746aaf99080e756427b204d3f2f9c445a480b58
Gulshan  Thakur <66913107+selfinception@users.noreply.github.com>
2020-06-23 21:27:01 +0530

    Create days0Solution.c

---
+++ days0Solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    
+    char inputstring[105]; 
+    
+    
+    scanf("%[^\n]", inputstring); 
+    
+
+    printf("Hello, World.\n");
+    
+
+    printf("%s",inputstring);
+    return 0;
+}
