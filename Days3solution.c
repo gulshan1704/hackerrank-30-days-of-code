2635ebd67a58fb78981ea6aabcde3bfbc5df703e
Gulshan  Thakur <66913107+selfinception@users.noreply.github.com>
2020-06-24 11:50:25 +0530

    Create Days3solution.c

---
+++ Days3solution.c
@@ -0,0 +1,19 @@
+if (N%2==0)
+    {
+        if(N>=2 && N<=5)
+        {
+            printf("Not Weird");
+        }
+        else if (N>=6 && N<=20)
+        {
+            printf("Weird");
+        }
+        else
+        {
+            printf("Not Weird");
+        }
+    }
+        else
+        {
+            printf("Weird");
+        }
